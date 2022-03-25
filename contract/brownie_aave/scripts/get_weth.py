c72abdb95ede66bc7c3f52b89f17303e4fc17154
YiMa0920 <yima2152@uni.sydney.edu.au>
2022-03-25 09:55:01 +0800

    update

---
+++ contract/brownie_aave/scripts/get_weth.py
@@ -0,0 +1,19 @@
+from scripts.helpful_scripts import get_account
+from brownie import interface, config, network, accounts
+import sys
+
+
+def main():
+    get_weth()
+
+
+def get_weth():
+    """
+    Mints WETH by depositing ETH.
+    """
+    account = get_account()
+    weth = interface.IWeth(config["networks"][network.show_active()]["weth_token"])
+    tx = weth.deposit({"from": account, "value": 0.001 * 10 ** 18})
+    tx.wait(1)
+    print("Received 0.1 WETH")
+    return tx
