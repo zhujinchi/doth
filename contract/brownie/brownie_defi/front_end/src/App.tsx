30213a3392cc447c08dffef9d7d89f80904af4b9
YiMa0920 <yima2152@uni.sydney.edu.au>
2022-04-11 10:50:59 +0800

    update

--- contract/brownie/brownie_defi/front_end/src/App.tsx
+++ contract/brownie/brownie_defi/front_end/src/App.tsx
@@ -6,21 +6,18 @@ import { Container } from "@material-ui/core"
 import { Main } from './components/Main'
 
 function App() {
-
   return (
     <DAppProvider config={{
-
-      supportedChains: [ChainId.Kovan]
-
+      supportedChains: [ChainId.Kovan],
+      notifications: {
+        expirationPeriod: 1000,
+        checkInterval: 1000
+      }
     }}>
       <Header />
-      <Container maxWidth="lg">
-        <div>
-          mayi
-        </div>
+      <Container maxWidth="md">
         <Main />
       </Container>
-
     </DAppProvider>
   )
 }
